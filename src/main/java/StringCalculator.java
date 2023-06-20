da74881f043c361d53a4fdfd2f1e77e5b963abe6
Yusa-Kaya <ozgur.yusa.kaya@gmail.com>
2023-06-20 10:39:39 -0700

    Add() method now throws illegalargumentexception when negative number is given and ignores when a number larger than or equal to 1000 is given

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -6,6 +6,11 @@ public class StringCalculator {
             throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
         } else {
             for (String number : numbersArray) {
+                if(Integer.parseInt(number.trim()) < 0){
+                    throw new IllegalArgumentException("Negatives are not allowed");
+                }else if(Integer.parseInt(number.trim()) >= 1000){
+                    continue;
+                }
                 if (!number.trim().isEmpty()) {
                     returnValue+=Integer.parseInt(number.trim()); // If it is not a number, parseInt will throw an exception
                 }
